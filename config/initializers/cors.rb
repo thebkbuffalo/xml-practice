056af966a3f6e97cf7f77feff63fb2c044a9db49
Evan Berg <evan.m.berg@gmail.com>
2022-07-14 21:32:18 -0400

    readded all urls to cors and changed credentials to false

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,18 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://abracadabrant-vin-50955.herokuapp.com/"
+    origins [
+      "localhost:3001",
+      "http://localhost:3001",
+      "http://127.0.0.1:3001", 
+      "127.0.0.1:3001",
+      "localhost:3000",
+      "https://abracadabrant-vin-50955.herokuapp.com/"
+    ]
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
 end
